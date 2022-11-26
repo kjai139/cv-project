18bddaa5ea739e8f20b383c2740f04c61fe69071
Simon Y <therealkjai139@gmail.com>
2022-11-25 20:29:36 -0500

    add remove functions exp

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,12 +1,128 @@
 import React, { Component } from "react";
+import format from "date-fns/format";
+import cBtn from './imgs/cancel-btn.svg'
 
 
 class Experience extends Component {
     constructor(prop){
         super(prop)
+        this.state = {
+            skills:['Creative thinking', 'Communication', 'Listening', 'Detailed-Oriented', 'Time Management', 'Problem Solving', 'Artistic Eye', 'Adaptability'],
+
+            credentials:[
+                {
+                    startDate: 'Jan 2019',
+                    endDate: 'Oct 2020',
+                    credTitle: 'Senior Web Developer',
+                    companyTitle: 'Abc Corporation',
+                    location: 'Toronto, ON',
+                    jobDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc ex, pharetra quis urna et, tincidunt gravida ante.'
+
+
+
+                },
+                {
+                    startDate: 'Jan 2018',
+                    endDate: 'Dec 2018',
+                    credTitle: 'Junior Web Developer',
+                    companyTitle: 'Abc Corporation',
+                    location: 'Toronto, ON',
+                    jobDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc ex, pharetra quis urna et, tincidunt gravida ante.'
+                }
+            ]
+        }
+
+        this.handleChangeSkills = this.handleChangeSkills.bind(this)
+        this.renderSkills = this.renderSkills.bind(this)
+        this.handleChangeExperiences = this.handleChangeExperiences.bind(this)
+        this.renderExperiences = this.renderExperiences.bind(this)
+
+        this.removeSkills = this.removeSkills.bind(this)
+        this.removeExperience = this.removeExperience.bind(this)
     }
 
+    handleChangeSkills(e){
+        e.preventDefault()
+        let data = new FormData(e.target)
+        let skillName =  data.get('skillName')
+        this.setState({skills:[...this.state.skills, skillName]})
 
+        e.target.reset()
+    }
+
+    handleChangeExperiences(e) {
+        e.preventDefault()
+        let data = new FormData(e.target)
+        let formatStart = format(new Date(`${data.get('startDate')}`.replace(/-/g, '\/').replace(/T.+/, '')),'MMM yyyy')
+
+        let formatEnd = format(new Date(`${data.get('endDate')}`.replace(/-/g, '\/').replace(/T.+/, '')),'MMM yyyy')
+
+
+        let obj = {
+            startDate: formatStart,
+            endDate: formatEnd,
+            credTitle: data.get('title'),
+            companyTitle: data.get('companyName'),
+            location: data.get('location'),
+            jobDescription: data.get('description')
+        }
+        
+        this.setState({credentials:[...this.state.credentials, obj]})
+
+        e.target.reset()
+    }
+
+    renderSkills() {
+        return this.state.skills.map((value, key) => 
+            
+            <li key={`skills-key-${key}`} id={`skill-${key}`} className="skillList">{value}<button className="listRemoveBtn" value={value} onClick={this.removeSkills}></button></li>
+        
+        
+        )
+    }
+
+    renderExperiences() {
+        return this.state.credentials.map((value, key) =>
+
+        <div className="credentials" key={`exp${key}`} id={`credentials-${key}`}>
+            <div className="credLeft">
+                <p className="dates">{value.startDate} - {value.endDate}</p>
+                <p className="credTitle">{value.credTitle}</p>
+            </div>
+            <div className="credRight">
+                <p className="companyTitle">{value.companyTitle}</p>
+                <p className="locationTxt">{value.location}</p>
+                <p className="description">{value.jobDescription}</p>
+            </div>
+            <button value={key} className="removeBtn" onClick={this.removeExperience}>
+                
+            </button>
+        </div>
+        )
+        
+    }
+
+    removeSkills(e) {
+        
+        let arr = this.state.skills
+        let index = this.state.skills.indexOf(e.target.value)
+        // console.log('remove', index, e.target.value)
+        let newArr = arr.filter((ele, eleIndex) => eleIndex !== index)
+        // console.log(newArr)
+
+        this.setState({skills: newArr})
+    }
+
+    removeExperience(e){
+        let arr = this.state.credentials
+        let index = Number(e.target.value)
+
+        let newArr = arr.filter((ele, eleIndex) => eleIndex !== index)
+
+        this.setState({credentials: newArr})
+
+
+    }
 
 
 
@@ -17,14 +133,14 @@ class Experience extends Component {
                 <div id="experienceLeft" className="btnShell">
                 <button className="addBtn">Add</button>
                     <div className="editFormDiv">
-                        <form id="skillsForm" name="skillsForm" className="editForm" autoComplete="off">
+                        <form id="skillsForm" name="skillsForm" className="editForm" autoComplete="off" onSubmit={this.handleChangeSkills}>
                             <label>
                                 Skill:
                                 <input type="text" name="skillName"></input>
                             </label>
                             <div className="submitBtnDiv">
                                 <button className="submitBtn">CANCEL</button>
-                                <button className="submitBtn">
+                                <button className="submitBtn" >
                                     OK
                                 </button>
                             </div>
@@ -35,14 +151,7 @@ class Experience extends Component {
                     <p className="headers sectionTitle">Skills</p>
                     <div className="borderLine"></div>
                     <ul id="skillList">
-                        <li>Creative thinking</li>
-                        <li>Communication</li>
-                        <li>Listening</li>
-                        <li>Detail-Oriented</li>
-                        <li>Time Management</li>
-                        <li>Problem Solving</li>
-                        <li>Artistic Eye</li>
-                        <li>Adaptability</li>
+                        {this.renderSkills()}
                         
                     </ul>
 
@@ -50,7 +159,7 @@ class Experience extends Component {
                 <div id="experienceRight" className="btnShell">
                 <button className="addBtn">Add</button>
                     <div className="editFormDiv">
-                        <form id="experienceForm" name="experienceForm" className="editForm" autoComplete="off">
+                        <form id="experienceForm" name="experienceForm" className="editForm" autoComplete="off" onSubmit={this.handleChangeExperiences}>
                             <label>
                                 Company:
                                 <input type="text" name="companyName"></input>
@@ -61,11 +170,11 @@ class Experience extends Component {
                             </label>
                             <label>
                                 Start Date:
-                                <input type="month" name="startDate"></input>
+                                <input required="yes" type="month" name="startDate"></input>
                             </label>
                             <label>
                                 End Date:
-                                <input type="month" name="endDate"></input>
+                                <input required="yes" type="month" name="endDate"></input>
                             </label>
                             <label>
                                 Location:
@@ -86,28 +195,8 @@ class Experience extends Component {
                     <div className="topBorderLine"></div>
                     <p className="headers">Experience</p>
                     <div className="borderLine"></div>
-                    <div className="credentials">
-                        <div className="credLeft">
-                            <p className="dates">Jan 2019 - Oct 2020</p>
-                            <p className="credTitle">Senior Web Developer</p>
-                        </div>
-                        <div className="credRight">
-                            <p className="companyTitle">ABC Corporation</p>
-                            <p className="locationTxt">Toronto,ON</p>
-                            <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc ex, pharetra quis urna et, tincidunt gravida ante.</p>
-                        </div>
-                    </div>
-                    <div className="credentials">
-                        <div className="credLeft">
-                            <p className="dates">Jan 2018 - Jan 2019</p>
-                            <p className="credTitle">Junior Web Developer</p>
-                        </div>
-                        <div className="credRight">
-                            <p className="companyTitle">ABC Corporation</p>
-                            <p className="locationTxt">Toronto,ON</p>
-                            <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc ex, pharetra quis urna et, tincidunt gravida ante.</p>
-                        </div>
-                    </div>
+                    {this.renderExperiences()}
+                    
 
                 </div>
 
